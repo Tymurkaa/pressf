aa5bd36029924d2b78682121b6af5ba82a30a214
Tymurkaa <timur16111978@gmail.com>
2024-10-22 17:06:36 +0200

    update

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ let registerBtn = document.querySelector('.registr-but');
         window.location.assign('registration.html');
     })
 
+    document.querySelector('.scroll-to-footer').addEventListener('click', function() {
+        document.getElementById('footer').scrollIntoView({ behavior: 'smooth' });
+    });
