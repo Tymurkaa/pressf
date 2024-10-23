f5e9bfe2c1a1757d8a456ba958f9e36e2fd09d8a
VikaKhaustova <haustovavika@gmail.com>
2024-10-23 15:56:06 +0300

    update

--- script.js
+++ script.js
@@ -3,28 +3,46 @@ let registerBtn = document.querySelector('.registr-but');
         window.location.assign('registration.html');
     })
 
-    document.querySelector('.scroll-to-footer').addEventListener('click', function() {
-        document.getElementById('footer').scrollIntoView({ behavior: 'smooth' });
+    // document.querySelector('.scroll-to-footer').addEventListener('click', function() {
+    //     document.getElementById('footer').scrollIntoView({ behavior: 'smooth' });
+    // });
+
+
+    // document.getElementById('themeToggle').addEventListener('change', function() {
+    //   document.body.classList.toggle('dark-theme', this.checked);
+    // });
+  
+    
+  
+    const themeToggle = document.getElementById("themeToggle");
+    const body = document.body;
+
+    themeToggle.addEventListener("change", () => {
+      if (themeToggle.checked) {
+        body.classList.add("light-theme");
+        body.classList.remove("dark-theme");
+      } else {
+        body.classList.add("dark-theme");
+        body.classList.remove("light-theme");
+      }
     });
 
+    // Сохранение темы при перезагрузке
+    document.addEventListener('DOMContentLoaded', () => {
+      const isLightTheme = localStorage.getItem('theme') === 'light';
+      if (isLightTheme) {
+        themeToggle.checked = true;
+        body.classList.add('light-theme');
+      } else {
+        themeToggle.checked = false;
+        body.classList.add('dark-theme');
+      }
+    });
 
-const toggle = document.getElementById('toggle');
-
-
-if (localStorage.getItem('dark-theme') === 'true') {
-    document.body.classList.add('dark-theme');
-    toggle.checked = true; 
-}
-
-toggle.addEventListener('change', function() {
-    if (this.checked) {
-        
-        document.body.classList.add('dark-theme');
-        localStorage.setItem('dark-theme', 'true'); 
-    } else {
-
-        document.body.classList.remove('dark-theme');
-        localStorage.setItem('dark-theme', 'false'); 
-    }
-});
-
+    themeToggle.addEventListener('change', () => {
+      if (themeToggle.checked) {
+        localStorage.setItem('theme', 'light');
+      } else {
+        localStorage.setItem('theme', 'dark');
+      }
+    });
